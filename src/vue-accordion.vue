a7356603a647cc3e019da43b0e52b0f949672d3a
ParroT <parrotdance95@gmail.com>
2018-04-16 12:23:12 +0800

    fix: some bugs; add: default expand image;

--- src/vue-accordion.vue
+++ src/vue-accordion.vue
@@ -1,59 +1,72 @@
 <template>
-	<div :class="accordionClass" :style="divStyles">
+	<div class="vue-accordion" :style="divStyles">
 		<ul>
 			<partial-accordion 
 				v-for="(item,index) in items" 
 				:item="item" 
-				:key="index"
+				:key="index" 
+				:index="index" 
+				:showIndex.sync="showIndex" 
 				:styles="styles"
+				@popup="popup"
 			></partial-accordion>
 		</ul>
 	</div>
 </template>
 <script>
-	import partialAccordion from './vue-accordion-partial.vue'
+	import partialAccordion from "./vue-accordion-partial.vue";
 
 	export default {
-		name: 'vue-accordion',
-		props: {
-			items: {
-				type: Array
-			},
-			styles: {
-				type: Object,
-				default: function() {
-					return {}
-				}
-			},
-			accordionClass: {
-				type: String,
-				default: 'vue-accordion'
+	  name: "vue-accordion",
+	  props: {
+	    items: {
+	      type: Array
+	    },
+	    styles: {
+	      type: Object,
+	      default: function() {
+	        return {};
+	      }
+	    },
+			defaultImage: {
+				type: Number,
+				default: 0
 			}
 		},
-		computed: {
-			divStyles() {
-				return this.styles && this.styles.div ? this.styles.div : {};
+		data () {
+			return {
+				showIndex: (this.defaultImage) ? this.defaultImage - 1 : NaN,
 			}
 		},
-		components: {
-			partialAccordion
-		}
-	}
+		methods: {
+			popup: function (val) {
+				this.showIndex = val;
+			}
+		},
+	  computed: {
+	    divStyles() {
+	      return this.styles && this.styles.div ? this.styles.div : {};
+			}
+	  },
+	  components: {
+	    partialAccordion
+	  }
+	};
 </script>
-<style>
+<style scoped>
 	.vue-accordion {
-		width: 100%;
-		overflow: hidden;
-		height: 200px;
-		max-width: 900px;
-    }
+	  width: 100%;
+	  overflow: hidden;
+	  height: 200px;
+	  max-width: 900px;
+	}
 
-    .vue-accordion ul {
-		width: 100%;
-		height: 100%;
-		display: table;
-		table-layout: fixed;
-		margin: 0;
-		padding: 0;
-    }
+	.vue-accordion ul {
+	  width: 100%;
+	  height: 100%;
+	  display: table;
+	  table-layout: fixed;
+	  margin: 0;
+	  padding: 0;
+	}
 </style>
